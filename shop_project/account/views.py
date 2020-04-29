63bf40b2a94b0ba83e089711a1d72dd661c8800a
wpoplaws <wojciechpoplawski94@gmail.com>
2020-04-29 13:46:12 +0200

    added login/logout panel

--- shop_project/account/views.py
+++ shop_project/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
@@ -24,3 +25,9 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+@login_required
+def main(request):
+    return render(request,
+                  'account/main.html',
+                  {'section': 'main'})
